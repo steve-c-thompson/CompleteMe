39823c44e1ee25b4ee59a97158ad40d21a52dc2a
Steve Thompson <sthompson@SDInstructor1.local>
2018-08-27 11:58:49 -0600

    Safety commit, no new functionality, but added some new code.

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -2,12 +2,12 @@ class CompleteMe
   attr_reader :count
   def initialize(opts = {})
     @count = 0
-    @words = []
+    @words = Node.new
   end
 
   class Node
     attr_reader :letter, :children
-    def initialize(letter, valid_word=false)
+    def initialize(letter=nil, valid_word=false)
       @letter = letter
       @children = []
       @valid = valid_word
@@ -16,9 +16,32 @@ class CompleteMe
     def valid?
       @valid
     end
+
+    def add_child(letters, begin_index, end_index)
+      if(letters.length > 0)
+        # if letters has only one letter, this is a valid node
+        child_node = get_child(letters[0])
+        if(child_node != nil)
+          # if this node is in children, don't add a new one, but call add_child
+
+        else
+          # add this node as a child
+
+        end
+
+        # return true if this succeeded in adding a new word
+      end
+    end
+
+    def get_child(letter)
+      nil
+    end
   end
 
   def insert(word)
-
+    chars = word.chars
+    if(@words.add_child(chars, 0, chars.length-1))
+      @count += 1
+    end
   end
 end
