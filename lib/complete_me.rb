af9d0e3a16fb6d142e26daed0e1734980bcd95ec
Steve Thompson <sthompson@SDInstructor1.local>
2018-08-28 21:01:39 -0600

    Fix bug adding multiple of a word

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -9,38 +9,56 @@ class CompleteMe
     attr_reader :letter, :children
     def initialize(letter=nil, valid_word=false)
       @letter = letter
-      @children = []
+      @children = {}
       @valid = valid_word
+    end
+
+    def to_s
+      "Node: #{@letter ? @letter : "nil"}, #{@children}, #{@valid}"
     end
 
     def valid?
       @valid
     end
 
-    def add_child(letters, begin_index, end_index)
-      if(letters.length > 0)
-        # if letters has only one letter, this is a valid node
-        child_node = get_child(letters[0])
-        if(child_node != nil)
-          # if this node is in children, don't add a new one, but call add_child
-
-        else
-          # add this node as a child
-
+    def add_child(letters, begin_index)
+      if(begin_index < letters.length)
+        # if letters has only one letter, this is a leaf node
+        leaf_node = false
+        if(begin_index + 1 == letters.length)
+          leaf_node = true
         end
+        node_letter = letters[begin_index]
 
-        # return true if this succeeded in adding a new word
+        child_node = get_child(node_letter)
+        if(child_node)
+          if (!leaf_node)
+            # if this node is in children, don't add a new one, but call add_child
+            return child_node.add_child(letters, begin_index + 1)
+          else
+            return false
+          end
+        else
+          # add this letter node as a child
+          child = Node.new(node_letter, leaf_node)
+          @children[node_letter] = child
+          if(leaf_node)
+            return true
+          else
+            return child.add_child(letters, begin_index + 1)
+          end
+        end
       end
     end
 
     def get_child(letter)
-      nil
+      @children[letter]
     end
   end
 
   def insert(word)
     chars = word.chars
-    if(@words.add_child(chars, 0, chars.length-1))
+    if(@words.add_child(chars, 0))
       @count += 1
     end
   end
--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -23,5 +23,21 @@ class CompleteMeTest < Minitest::Test
     assert_equal 1, complete_me.count
   end
 
+  def test_it_does_not_insert_duplicaates_and_update_word_count
+    complete_me = CompleteMe.new
+    complete_me.insert("pizza")
+    complete_me.insert("pizza")
+
+    assert_equal 1, complete_me.count
+  end
+
+  def test_it_can_suggest_items_in_dictionary
+    skip
+    complete_me = CompleteMe.new
+    complete_me.insert("pizza")
+
+    sug = complete_me.suggest("piz")
+    assert_equal ["pizza"], sug
+  end
 
 end
